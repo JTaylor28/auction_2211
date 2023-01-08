76b3f6377c71c4e4accc9ebcaa6f608993de05d9
James Taylor <jrtaylor0614@gmail.com>
2023-01-08 11:13:00 -0700

    Feat: initial git

---
+++ spec/item_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Item do 
+    let(:item1) do 
+        Item.new('Chalkware Piggy Bank')
+    end
+
+    describe"#initialize" do 
+        it"exsits" do
+            expect(item1).to be_an_instance_of(Item)
+        end
+    end
+
+    describe" it has attributes" do
+        it "has a name" do
+            expect(item1.name).to eq('Chalkware Piggy Bank')
+        end
+        
+        it "#bids" do
+            expect(item1.bids).to eq({})
+        end
+    end
+end
